f784b43e1c962dd24191f9ef059b3fb6b249f56d
Gaurab Subedi <gaurabsubedi@Gaurabs-MacBook-Air.local>
2025-04-30 17:01:57 +0545

    text input component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { View, Text} from 'react-native';
 import * as React from 'react';
 import WelcomeScreen from './welcomescreen';
+// import FeedbackForm from './FeedBackForm';
 // import MenuItems from './MenuItems';
 
 
@@ -9,7 +10,7 @@ export default function HomeScreen() {
     <View style={{ flex: 1, backgroundColor: "blue"}}> 
     <WelcomeScreen/>
     {/* <MenuItems/> */}
-    
+    {/* <FeedbackForm/> */}
     </View>
   );
 }
