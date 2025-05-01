314e7540da48620f6ceaca6bcbe27f20941dab00
Gaurab Subedi <gaurabsubedi@Gaurabs-MacBook-Air.local>
2025-05-01 11:05:52 +0545

    login UI learn

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,18 @@
 import { View, Text} from 'react-native';
 import * as React from 'react';
-import WelcomeScreen from './welcomescreen';
+// import WelcomeScreen from './welcomescreen';
 // import FeedbackForm from './FeedBackForm';
+import LoginScreen from './LoginScreen';
 // import MenuItems from './MenuItems';
 
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, backgroundColor: "blue"}}> 
-    <WelcomeScreen/>
+    <View style={{ flex: 1}}> 
+    {/* <WelcomeScreen/> */}
     {/* <MenuItems/> */}
     {/* <FeedbackForm/> */}
+    <LoginScreen/>
     </View>
   );
 }
